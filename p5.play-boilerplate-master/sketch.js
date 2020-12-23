f0341516ae7678760b96efd4618ce9b947e55f56
WizenWhistle33 <72667184+WizenWhistle33@users.noreply.github.com>
2020-12-23 15:55:01 -0800

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,28 @@
+var blackcar,bluecar,green,car,redcare;
+function preload(){
+  blackcar.createSprite(400, 100, 50, 50);
+  bluekcar.createSprite(400, 150, 50, 50);
+  greencar.createSprite(400, 300, 50, 50);
+  redcar.createSprite(400, 250, 50, 50);
+}
+
+
+
+function setup() {
+  createCanvas(800,400);
+  blackcar.velocityx=10
+  bluecar.velocityx=11;
+  greencar.velocityx=12;
+  redcar.velocityx=20;
+}
+
+
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  if redcar.istouching wall 1;
+  redcar velocityx=0;
+
+  
+}
